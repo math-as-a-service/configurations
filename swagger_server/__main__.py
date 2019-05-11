92f8cb18df3a294553b829b50b7eba9366878e1e
Alex Bi <alex.bi@klaviyo.com>
2019-05-11 12:52:46 -0400

    routes for evaluation and result

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+import flask
 
-from swagger_server.controllers.expression_controller import add_expression
+from swagger_server.controllers.expression_controller import add_expression, get_expression
+from swagger_server.controllers.evaluation_controller import add_evaluation, get_evaluation
+from swagger_server.controllers.result_controller import get_result
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+
 
 @app.route('/expression', methods=['POST'])
 def post_expression_view():
     return add_expression()
 
+
 @app.route('/expression', methods=['GET'])
 def get_expression_view():
-    return get_expression()
+    expression_id = flask.request.json['expression_id']
+    return get_expression(expression_id)
+
+
+@app.route('/evaluation', methods=['POST'])
+def post_evaluation_view():
+    return add_evaluation()
+
+
+@app.route('/evaluation', methods=['GET'])
+def get_evaluation_view():
+    evaluation_id = flask.request.json['evaluation_id']
+    return get_evaluation(evaluation_id)
+
+
+@app.route('/result', methods=['GET'])
+def get_result_view():
+    return get_result()
