ae4df8c7907b2167e535a56ad6157c359d67b504
Chris Conlon <chris.conlon@klaviyo.com>
2019-05-11 12:33:51 -0400

    add more stuff

--- commands/create_tables.py
+++ commands/create_tables.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
-# import all models
-# loop over them and call `create_table` as described here http://docs.peewee-orm.com/en/2.10.2/peewee/models.html?highlight=create_table#creating-model-tables
+from swagger_server.models import (
+    Evaluation, EvaluationStatus, Expression, Operand, Operator, Result, ValueType,
+)
+
+MODEL_CONSTRUCTORS = [
+    Evaluation,
+    EvaluationStatus,
+    Expression,
+    Operand,
+    Operator,
+    Result,
+    ValueType,
+]
+
+# http://docs.peewee-orm.com/en/2.10.2/peewee/models.html?highlight=create_table#creating-model-tables
+for model in MODEL_CONSTRUCTORS:
+    model.create_table()
